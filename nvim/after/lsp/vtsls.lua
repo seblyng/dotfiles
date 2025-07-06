80baa5f96d4fdc7b9632b86959bdc152ff64c54b
Sebastian Lyng Johansen <sebastian@lyngjohansen.com>
2025-07-06 19:19:46 +0200

    Try out vtsls and setup for vue_ls 3.0

---
+++ nvim/after/lsp/vtsls.lua
@@ -0,0 +1,25 @@
+return {
+    filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+    settings = {
+        vtsls = {
+            tsserver = {
+                globalPlugins = {
+                    {
+                        name = "@vue/typescript-plugin",
+                        location = vim.fs.joinpath(
+                            vim.fn.stdpath("data"),
+                            "mason",
+                            "packages",
+                            "vue-language-server",
+                            "node_modules",
+                            "@vue",
+                            "language-server"
+                        ),
+                        languages = { "vue" },
+                        configNamespace = "typescript",
+                    },
+                },
+            },
+        },
+    },
+}
