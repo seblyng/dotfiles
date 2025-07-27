3e59068ea8e20cd8403c80e6683210011ff1554c
Sebastian Lyng Johansen <sebastian@lyngjohansen.com>
2025-07-27 19:26:36 +0200

    remove unnecessary vue_ls config
    however, need to have this hack as a workaround because of some
    mason-lspconfig bug...

--- nvim/after/lsp/vue_ls.lua
+++ nvim/after/lsp/vue_ls.lua
@@ -1,22 +1,8 @@
 ---@type vim.lsp.Config
 return {
-    on_init = function(client)
-        client.handlers["tsserver/request"] = function(_, result, ctx)
-            local ts_client = vim.lsp.get_clients({ bufnr = ctx.bufnr, name = "vtsls" })[1]
-            if ts_client == nil then
-                vim.notify("Could not find `vtsls`. Required for `vue_ls` to work.", vim.log.levels.ERROR)
-                return
-            end
-
-            local param = unpack(result)
-            local id, command, payload = unpack(param)
-            ts_client:exec_cmd({
-                title = "vue_request_forward",
-                command = "typescript.tsserverRequest",
-                arguments = { command, payload },
-            }, { bufnr = ctx.bufnr }, function(_, r)
-                client:notify("tsserver/response", { { id, r.body } })
-            end)
-        end
-    end,
+    init_options = {
+        typescript = {
+            tsdk = "",
+        },
+    },
 }
