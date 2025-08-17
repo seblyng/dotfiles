92c53318794bdd5818c0d4b8fbadea2947c80c4f
Sebastian Lyng Johansen <sebastian@lyngjohansen.com>
2025-08-17 10:52:09 +0200

    Implement fidget integration to roslyn dotnet restore

--- nvim/lua/config/dotnet.lua
+++ nvim/lua/config/dotnet.lua
@@ -23,5 +23,42 @@ return {
 
             Snacks.picker.files({ dirs = files })
         end)
+
+        local handles = {}
+
+        vim.api.nvim_create_autocmd("User", {
+            pattern = "RoslynRestoreProgress",
+            callback = function(ev)
+                local token = ev.data.params[1]
+                local handle = handles[token]
+                if handle then
+                    handle:report({
+                        title = ev.data.params[2].state,
+                        message = ev.data.params[2].message,
+                    })
+                else
+                    handles[token] = require("fidget.progress").handle.create({
+                        title = ev.data.params[2].state,
+                        message = ev.data.params[2].message,
+                        lsp_client = {
+                            name = "roslyn",
+                        },
+                    })
+                end
+            end,
+        })
+
+        vim.api.nvim_create_autocmd("User", {
+            pattern = "RoslynRestoreResult",
+            callback = function(ev)
+                local handle = handles[ev.data.token]
+                handles[ev.data.token] = nil
+
+                if handle then
+                    handle.message = ev.data.err and ev.data.err.message or "Restore completed"
+                    handle:finish()
+                end
+            end,
+        })
     end,
 }
