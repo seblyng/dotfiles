f235a37acb8c6134478987d3d89db7664f071f1e
Sebastian Lyng Johansen <sebastian@lyngjohansen.com>
2025-08-21 18:49:51 +0200

    Use silent option for roslyn and implement fidget notifications
    Sort of hacky manual progress implementation for roslyn

--- nvim/lua/config/dotnet.lua
+++ nvim/lua/config/dotnet.lua
@@ -2,6 +2,7 @@ return {
     "seblyng/roslyn.nvim",
     opts = {
         broad_search = true,
+        silent = true,
         ignore_target = function(sln)
             return string.match(sln, "SmartDok.sln") ~= nil
         end,
@@ -23,20 +24,20 @@ return {
             Snacks.picker.files({ dirs = files })
         end)
 
-        local handles = {}
+        local restore_handles = {}
 
         vim.api.nvim_create_autocmd("User", {
             pattern = "RoslynRestoreProgress",
             callback = function(ev)
                 local token = ev.data.params[1]
-                local handle = handles[token]
+                local handle = restore_handles[token]
                 if handle then
                     handle:report({
                         title = ev.data.params[2].state,
                         message = ev.data.params[2].message,
                     })
                 else
-                    handles[token] = require("fidget.progress").handle.create({
+                    restore_handles[token] = require("fidget.progress").handle.create({
                         title = ev.data.params[2].state,
                         message = ev.data.params[2].message,
                         lsp_client = {
@@ -50,11 +51,40 @@ return {
         vim.api.nvim_create_autocmd("User", {
             pattern = "RoslynRestoreResult",
             callback = function(ev)
-                local handle = handles[ev.data.token]
-                handles[ev.data.token] = nil
+                local handle = restore_handles[ev.data.token]
+                restore_handles[ev.data.token] = nil
 
                 if handle then
                     handle.message = ev.data.err and ev.data.err.message or "Restore completed"
+                    handle:finish()
+                end
+            end,
+        })
+
+        local init_handles = {}
+        vim.api.nvim_create_autocmd("User", {
+            pattern = "RoslynOnInit",
+            callback = function(ev)
+                init_handles[ev.data.client_id] = require("fidget.progress").handle.create({
+                    title = "Initializing Roslyn",
+                    message = ev.data.type == "solution"
+                            and string.format("Initializing Roslyn for %s", ev.data.target)
+                        or "Initializing Roslyn for project",
+                    lsp_client = {
+                        name = "roslyn",
+                    },
+                })
+            end,
+        })
+
+        vim.api.nvim_create_autocmd("User", {
+            pattern = "RoslynInitialized",
+            callback = function(ev)
+                local handle = init_handles[ev.data.client_id]
+                init_handles[ev.data.client_id] = nil
+
+                if handle then
+                    handle.message = "Roslyn initialized"
                     handle:finish()
                 end
             end,
