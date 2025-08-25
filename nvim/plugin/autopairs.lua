b472f1f7c9091303329d336fb43aaf0083e86fc2
Sebastian Lyng Johansen <sebastian@lyngjohansen.com>
2025-08-25 20:55:29 +0200

    Update autopairs

--- nvim/plugin/autopairs.lua
+++ nvim/plugin/autopairs.lua
@@ -38,7 +38,16 @@ local function between_quotes()
         or (char_before == "`" and char_after == "`")
 end
 
-local function try_insert_match(typed)
+local function try_skip(typed)
+    local _, char_after = get_chars()
+    if char_after == typed then
+        feed("<Right><BS>")
+        return true
+    end
+    return false
+end
+
+local function try_insert_bracket(typed)
     local closing = matches[typed]
     local char_before, char_after = get_chars()
 
@@ -46,46 +55,28 @@ local function try_insert_match(typed)
         return feed(string.format("%s<Left>", closing))
     end
 
-    if char_after == "]" or char_after == "}" or char_after == ")" then
+    if vim.list_contains({ "]", "}", ")" }, char_after) then
         return feed(string.format("%s<Left>", closing))
     end
 
-    if char_before and char_before:match("%s") and char_after:match("%s") then
-        return feed(string.format("%s<Left>", closing))
-    end
-end
-
-local function try_skip(typed)
-    local _, char_after = get_chars()
-    if char_after == typed then
-        feed("<Right><BS>")
+    if char_after:match("%s") then
+        if char_before and char_before:match("[%s%w]") then
+            return feed(string.format("%s<Left>", closing))
+        end
     end
 end
 
 local function try_insert_quote(typed)
+    local closing = matches[typed]
     local char_before, char_after = get_chars()
 
-    if char_before and char_before:match("%s") and char_after and char_after:match("%s") then
-        return try_insert_match(typed)
+    if (not char_before or char_before:match("%s")) and (not char_after or char_after:match("%s")) then
+        return feed(string.format("%s<Left>", closing))
     end
 
-    if not char_before and not char_after then
-        return try_insert_match(typed)
+    if vim.list_contains({ "[", "{", "(" }, char_before) or vim.list_contains({ "]", "}", ")" }, char_after) then
+        return feed(string.format("%s<Left>", closing))
     end
-
-    if not char_before and char_after and char_after:match("%s") then
-        return try_insert_match(typed)
-    end
-
-    if char_before and char_before:match("%s") and not char_after then
-        return try_insert_match(typed)
-    end
-
-    if char_before == "[" or char_before == "{" or char_before == "(" then
-        return try_insert_match(typed)
-    end
-
-    return try_skip(typed)
 end
 
 vim.on_key(function(_, typed)
@@ -97,19 +88,26 @@ vim.on_key(function(_, typed)
         return
     end
 
-    if typed == "(" or typed == "{" or typed == "[" then
-        try_insert_match(typed)
-    elseif typed == ")" or typed == "}" or typed == "]" then
+    if vim.list_contains({ "(", "{", "[" }, typed) then
+        try_insert_bracket(typed)
+    elseif vim.list_contains({ ")", "}", "]" }, typed) then
         try_skip(typed)
-    elseif typed == '"' or typed == "'" or typed == "`" then
-        try_insert_quote(typed)
+    elseif vim.list_contains({ "'", '"', "`" }, typed) then
+        if not try_skip(typed) then
+            try_insert_quote(typed)
+        end
     elseif vim.fn.keytrans(typed) == "<CR>" then
         if between_brackets() or between_quotes() then
-            feed("<Esc>O")
+            return feed("<Esc>O")
         end
-    elseif vim.fn.keytrans(typed) == "<BS>" then
+
+        local char_before, char_after = get_chars()
+        if char_before == ">" and char_after == "<" then
+            return feed("<Esc>O")
+        end
+    elseif vim.fn.keytrans(typed) == "<BS>" or vim.fn.keytrans(typed) == "<S-BS>" then
         if between_brackets() or between_quotes() then
-            feed("<Right><BS>")
+            return feed("<Right><BS>")
         end
     end
 end)
