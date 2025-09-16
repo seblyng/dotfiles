dfa561daacdbbd5f6a5460a40134de68a42d9c3d
Sebastian Lyng Johansen <sebastian@lyngjohansen.com>
2025-09-16 16:37:56 +0200

    updates

--- nvim/lua/seblyng/globals.lua
+++ nvim/lua/seblyng/globals.lua
@@ -1,18 +1,3 @@
 P = function(...)
     vim.print(...)
 end
-
--- I know that these are deprecated, I just don't want the warning all the time
--- Do a very big hack until the plugins I use updates to newer API's
-local deprecations = {
-    "client.notify",
-}
-
-local deprecate = vim.deprecate
----@diagnostic disable-next-line: duplicate-set-field
-vim.deprecate = function(name, ...)
-    if vim.list_contains(deprecations, name) then
-        return nil
-    end
-    return deprecate(name, ...)
-end
