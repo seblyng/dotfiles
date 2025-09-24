41e2155174b44b7326ee90ad9e6e53af37365d11
Sebastian Lyng Johansen <sebastian@lyngjohansen.com>
2025-09-24 23:25:45 +0200

    delete a bunch of things I don't need anymore

--- scripts/replace_icons.sh
+++
@@ -1,5 +0,0 @@
-fileicon set /Applications/Plex.app $HOME/dotfiles/icons/Plex.icns
-fileicon set /Applications/kitty.app $HOME/dotfiles/icons/kitty.icns
-fileicon set /Applications/reMarkable.app $HOME/dotfiles/icons/reMarkable.icns
-
-sudo killall Finder && sudo killall Dock
