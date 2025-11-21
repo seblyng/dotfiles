36d9866dfe59a98a34b3ce370cc130c0fb3bebac
Sebastian Lyng Johansen <sebastian@lyngjohansen.com>
2025-11-21 08:16:37 +0100

    Use commands for keymap with gitsigns
    Avoid disabling stylua to keep them on one line. This works just as fine

--- nvim/lua/config/gitsigns.lua
+++ nvim/lua/config/gitsigns.lua
@@ -2,17 +2,13 @@ return {
     "lewis6991/gitsigns.nvim",
     opts = {
         on_attach = function(bufnr)
-            local gs = require("gitsigns")
-
-            -- stylua: ignore start
-            vim.keymap.set("n", "]c", function() gs.nav_hunk("next") end, { buffer = bufnr, desc = "Gitsigns: Go to next diff hunk" })
-            vim.keymap.set("n", "[c", function() gs.nav_hunk("prev") end, { buffer = bufnr, desc = "Gitsigns: Go to previous diff hunk" })
-            vim.keymap.set("n", "<leader>gm", gs.blame_line, { buffer = bufnr, desc = "Gitsigns: Git blame current line" })
-            vim.keymap.set("n", "<leader>gb", gs.blame, { buffer = bufnr, desc = "Gitsigns: Git blame entire file" })
-            vim.keymap.set("n", "<leader>gd", gs.preview_hunk, { buffer = bufnr, desc = "Gitsigns: Preview diff hunk" })
-            vim.keymap.set("n", "<leader>grh", gs.reset_hunk, { buffer = bufnr, desc = "Gitsigns: Reset diff hunk over cursor" })
-            vim.keymap.set("n", "<leader>grb", gs.reset_buffer, { buffer = bufnr, desc = "Gitsigns: Reset diff for entire buffer" })
-            -- stylua: ignore end
+            vim.keymap.set("n", "]c", "<cmd>Gitsigns nav_hunk next<CR>", { buffer = bufnr })
+            vim.keymap.set("n", "[c", "<cmd>Gitsigns nav_hunk prev<CR>", { buffer = bufnr })
+            vim.keymap.set("n", "<leader>gm", "<cmd>Gitsigns blame_line<CR>", { buffer = bufnr })
+            vim.keymap.set("n", "<leader>gb", "<cmd>Gitsigns blame<CR>", { buffer = bufnr })
+            vim.keymap.set("n", "<leader>gd", "<cmd>Gitsigns preview_hunk<CR>", { buffer = bufnr })
+            vim.keymap.set("n", "<leader>grh", "<cmd>Gitsigns reset_hunk<CR>", { buffer = bufnr })
+            vim.keymap.set("n", "<leader>grb", "<cmd>Gitsigns reset_buffer<CR>", { buffer = bufnr })
         end,
     },
 }
