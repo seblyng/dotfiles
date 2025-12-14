9c17efad7419796ffd39afc2635468a0ba285117
Sebastian Lyng Johansen <sebastian@lyngjohansen.com>
2025-12-14 21:57:06 +0100

    updates

--- nvim/plugin/plugins.lua
+++ nvim/plugin/plugins.lua
@@ -27,7 +27,7 @@ vim.pack.add({
     { src = "https://github.com/mason-org/mason-lspconfig.nvim" },
     { src = "https://github.com/mason-org/mason.nvim" },
     { src = "https://github.com/folke/lazydev.nvim", data = { opts = {} } },
-    { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main", data = { build = ":TSUpdate" } },
+    { src = "https://github.com/nvim-treesitter/nvim-treesitter", data = { build = ":TSUpdate" } },
     { src = "https://github.com/nvim-treesitter/nvim-treesitter-textobjects", version = "main" },
     { src = "https://github.com/lewis6991/gitsigns.nvim" },
 })
@@ -64,18 +64,12 @@ require("oil").setup({
     view_options = { show_hidden = true },
 })
 
-local function map_select(mode, lhs, query)
-    vim.keymap.set(mode, lhs, function()
-        require("nvim-treesitter-textobjects.select").select_textobject(query, "textobjects")
-    end)
-end
-
-map_select({ "x", "o" }, "if", "@function.inner")
-map_select({ "x", "o" }, "af", "@function.outer")
-map_select({ "x", "o" }, "ic", "@class.inner")
-map_select({ "x", "o" }, "ac", "@class.outer")
-
 -- stylua: ignore start
+vim.keymap.set({ "x", "o" }, "if", function() require("nvim-treesitter-textobjects.select").select_textobject("@function.inner") end)
+vim.keymap.set({ "x", "o" }, "af", function() require("nvim-treesitter-textobjects.select").select_textobject("@function.outer") end)
+vim.keymap.set({ "x", "o" }, "ic", function() require("nvim-treesitter-textobjects.select").select_textobject("@class.inner") end)
+vim.keymap.set({ "x", "o" }, "ac", function() require("nvim-treesitter-textobjects.select").select_textobject("@class.outer") end)
+
 vim.keymap.set("n", "<leader>sa", function() require("nvim-treesitter-textobjects.swap").swap_next("@parameter.inner") end)
 vim.keymap.set("n", "<leader>sf", function() require("nvim-treesitter-textobjects.swap").swap_next("@function.outer") end)
 vim.keymap.set("n", "<leader>sA", function() require("nvim-treesitter-textobjects.swap").swap_previous("@parameter.inner") end)
