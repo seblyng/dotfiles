2d9be632029f37e47ae73d89f56ba9600e64dbe4
Sebastian Lyng Johansen <sebastian@lyngjohansen.com>
2026-05-06 22:11:36 +0200

    remove invalid bash syntax from install script
    just remove the entire line. it is done in .zshrc so just open a new
    shell

--- install/packages.sh
+++ install/packages.sh
@@ -24,8 +24,6 @@ fi
 
 install_packages "${packages[@]}"
 
-source <(fzf --zsh)
-
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
 $HOME/.cargo/bin/cargo install git-delta
