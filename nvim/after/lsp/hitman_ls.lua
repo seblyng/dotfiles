ab480c14674ff9ba81a259d69fe9eaf1b7d09b3e
Sebastian Lyng Johansen <sebastian@lyngjohansen.com>
2026-06-30 11:38:37 +0200

    Simplify hitman_ls code

--- nvim/after/lsp/hitman_ls.lua
+++ nvim/after/lsp/hitman_ls.lua
@@ -1,52 +1,39 @@
-local function execute_command(client, bufnr, command, arguments, callback)
-    client:request("workspace/executeCommand", {
-        command = command,
-        arguments = arguments,
-    }, function(err, result)
-        if err then
-            vim.notify(err.message, vim.log.levels.ERROR, { title = "hitman-lsp" })
-            return
-        end
-
-        callback(result)
-    end, bufnr)
-end
-
 ---@type vim.lsp.Config
 return {
     cmd = { "hitman-lsp" },
-    filetypes = { "http" },
+    filetypes = { "http", "graphql" },
     root_markers = { "hitman.toml" },
     commands = {
-        ["hitman.selectGrpcMethod"] = function(cmd, ctx)
+        ["hitman.selectGrpcMethod"] = function(data, ctx)
             local client = assert(vim.lsp.get_client_by_id(ctx.client_id))
             local bufnr = assert(ctx.bufnr)
-            local args = cmd.arguments and cmd.arguments[1] or {}
+            local args = data.arguments and data.arguments[1] or {}
             local uri = args.uri or vim.uri_from_bufnr(bufnr)
+            local methods = args.methods or {}
 
-            execute_command(client, bufnr, "hitman.listGrpcMethods", { { uri = uri } }, function(methods)
-                if not methods or vim.tbl_isempty(methods) then
-                    vim.notify("No gRPC methods found", vim.log.levels.WARN, { title = "hitman-lsp" })
+            if vim.tbl_isempty(methods) then
+                return vim.notify("No gRPC methods found", vim.log.levels.WARN, { title = "hitman-lsp" })
+            end
+
+            vim.ui.select(methods, {
+                prompt = "gRPC method",
+                format_item = function(item)
+                    return item.label
+                end,
+            }, function(item)
+                if not item then
                     return
                 end
 
-                vim.ui.select(methods, {
-                    prompt = "gRPC method",
-                    format_item = function(item)
-                        return item.label
-                    end,
-                }, function(item)
-                    if not item then
-                        return
-                    end
-
-                    execute_command(client, bufnr, "hitman.setGrpcMethod", {
+                client:request("workspace/executeCommand", {
+                    command = "hitman.setGrpcMethod",
+                    arguments = {
                         {
                             uri = uri,
                             method = item.method,
                         },
-                    })
-                end)
+                    },
+                }, nil, bufnr)
             end)
         end,
     },
