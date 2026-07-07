8323188f040bd2e99116a9aa5f61e5ca4e394055
Sebastian Lyng Johansen <sebastian@lyngjohansen.com>
2026-07-07 22:49:14 +0200

    updates

--- nvim/plugin/ui.lua
+++ nvim/plugin/ui.lua
@@ -6,24 +6,6 @@ local options = {
     max_width = 80,
 }
 
-local function set_close_mapping(key)
-    vim.keymap.set("n", key, function()
-        vim.api.nvim_win_close(0, true)
-    end, {
-        buf = 0,
-        desc = "Close popup",
-    })
-end
-
-local function set_highlight(buf, title, width)
-    vim.api.nvim_buf_set_extmark(buf, ns, 0, 0, { end_col = #title, hl_group = "Title" })
-    vim.api.nvim_buf_set_extmark(buf, ns, 1, 0, {
-        virt_text_win_col = 0,
-        virt_text = { { string.rep(options.border_line, width), "@punctuation.special.markdown" } },
-        priority = 100,
-    })
-end
-
 ---@diagnostic disable-next-line: duplicate-set-field
 vim.ui.input = function(opts, on_confirm)
     vim.schedule(function()
@@ -41,8 +23,12 @@ vim.ui.input = function(opts, on_confirm)
 
         vim.api.nvim_set_current_win(winnr)
 
-        set_close_mapping("<Esc>")
-        set_close_mapping("q")
+        vim.keymap.set("n", "<Esc>", function()
+            vim.api.nvim_win_close(0, true)
+        end, {
+            buf = popup_bufnr,
+            desc = "Close popup",
+        })
 
         vim.keymap.set({ "i", "n" }, "<CR>", function()
             local input = vim.trim(vim.fn.getline("."):sub(#options.prefix + 1, -1))
@@ -50,7 +36,7 @@ vim.ui.input = function(opts, on_confirm)
             on_confirm(input)
             vim.cmd.stopinsert()
         end, {
-            buf = 0,
+            buf = popup_bufnr,
             desc = "Confirm selection",
         })
 
@@ -61,7 +47,12 @@ vim.ui.input = function(opts, on_confirm)
         if opts.default and #opts.default ~= 0 then
             vim.api.nvim_input(string.format("%s<Esc>0wv$h<C-g>", opts.default))
         end
-        set_highlight(popup_bufnr, lines[1], width)
+        vim.api.nvim_buf_set_extmark(popup_bufnr, ns, 0, 0, { end_col = #lines[1], hl_group = "Title" })
+        vim.api.nvim_buf_set_extmark(popup_bufnr, ns, 1, 0, {
+            virt_text_win_col = 0,
+            virt_text = { { string.rep(options.border_line, width), "@punctuation.special.markdown" } },
+            priority = 100,
+        })
         vim.api.nvim_buf_set_extmark(popup_bufnr, ns, 2, 0, {
             virt_text_win_col = 0,
             virt_text = { { options.prefix, "Title" } },
